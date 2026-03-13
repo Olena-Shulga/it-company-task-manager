91f2e32f4687997d4c41088b4b908d37bf39bc1f
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-13 15:39:06 +0200

    add Worker model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Position(models.Model):
@@ -18,3 +19,18 @@ class TaskType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+    )
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
