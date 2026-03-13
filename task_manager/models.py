a79dd4758e39b9bd586d8461b7b8402db9f8520a
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-13 15:32:40 +0200

    add TaskType model

--- task_manager/models.py
+++ task_manager/models.py
@@ -8,3 +8,13 @@ class Position(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
