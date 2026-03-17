fd5bd242a12bbf88eec0b7fcb7e43c1a9f3847ea
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-17 18:14:01 +0200

    add 'position-list' url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import index, PositionListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "positions/", PositionListView.as_view(), name="position-list"
+    ),
+
 ]
 
 app_name = "task_manager"
