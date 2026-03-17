c8cde2b99992ee9864ff7f7758be7caff18414a3
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-17 18:21:01 +0200

    add 'position-view' url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from task_manager.views import index, PositionListView
+from task_manager.views import index, PositionListView, PositionDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path(
         "positions/", PositionListView.as_view(), name="position-list"
+    ),
+    path(
+        "positions/<int:pk>/",
+        PositionDetailView.as_view(),
+        name="position-view"
     ),
 
 ]
