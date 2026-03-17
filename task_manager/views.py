770a1df7a0aeac47f9e18a812ad8b1556d2add12
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-17 14:11:16 +0200

    add index view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Position, TaskType, Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_workers = get_user_model().objects.count()
+    num_positions = Position.objects.count()
+    num_task_types = TaskType.objects.count()
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_positions": num_positions,
+        "num_task_types": num_task_types,
+        "num_tasks": num_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
