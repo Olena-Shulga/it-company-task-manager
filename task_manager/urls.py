228e1413ec9b4b31b02de3c94f8e0286deb403af
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-18 16:23:15 +0200

    add 'task-type-update' url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from task_manager.views import index, PositionListView, PositionDetailView, PositionCreateView, PositionUpdateView, \
-    TaskTypeListView, PositionDeleteView, TaskTypeDetailView, TaskTypeCreateView
+    TaskTypeListView, PositionDeleteView, TaskTypeDetailView, TaskTypeCreateView, TaskTypeUpdateView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -44,6 +44,11 @@ urlpatterns = [
         "task_types/create/",
         TaskTypeCreateView.as_view(),
         name="task-type-create"
+    ),
+    path(
+        "task_types/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update"
     ),
 ]
 
