9cd568e56a597e8a89d07a5266eb76a3f88e7c41
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-03-20 16:10:00 +0200

    add django-degub-toolbar to the project

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -38,11 +41,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
+    "crispy_bootstrap4",
+    "crispy_forms",
     "task_manager"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,3 +131,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "task_manager.Worker"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
