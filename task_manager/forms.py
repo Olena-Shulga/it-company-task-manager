4feff8f958ff43f77fb18a72a572af26adf90e6a
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-04-21 08:56:50 +0300

    add TaskSearchForm, WorkerUpdateForm, WorkerSearchForm

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from task_manager.models import Task, TaskType
+from task_manager.models import Task, TaskType, Position
 
 
 def validate_deadline(value):
@@ -28,6 +28,34 @@ class TaskForm(forms.ModelForm):
         return validate_deadline(self.cleaned_data["deadline"])
 
 
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+            max_length=255,
+            required=False,
+            label="",
+            widget=forms.TextInput(
+                attrs={"placeholder": "Search by name"}
+            )
+        )
+    priority = forms.ChoiceField(
+        choices=(
+            ("", "----------"),
+            ("U", "Urgent"),
+            ("H", "High"),
+            ("M", "Medium"),
+            ("L", "Low"),
+        ),
+        required=False,
+        label="Priority",
+        widget=forms.Select,
+    )
+    task_type = forms.ModelChoiceField(
+        queryset=TaskType.objects.all(),
+        required=False,
+        widget=forms.Select,
+    )
+
+
 class WorkerForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
@@ -35,4 +63,50 @@ class WorkerForm(UserCreationForm):
             "first_name",
             "last_name",
             "position"
+        )
+
+
+class WorkerUpdateForm(forms.ModelForm):
+    username = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    position = forms.ModelChoiceField(queryset=Position.objects.all(), widget=forms.RadioSelect)
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "position",
+        ]
+
+    template_name = "task_manager/worker_form.html"
+
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+            max_length=255,
+            required=False,
+            label="",
+            widget=forms.TextInput(
+                attrs={"placeholder": "Search by username"}
+            )
+        )
+    position = forms.ModelChoiceField(
+        queryset=Position.objects.all(),
+        required=False,
+        label="Position:",
+        widget=forms.Select,
+    )
+
+
+class SearchForm(forms.Form):
+    name = forms.CharField(
+            max_length=255,
+            required=False,
+            label="",
+            widget=forms.TextInput(
+                attrs={"placeholder": "Search by name"}
+            )
         )
